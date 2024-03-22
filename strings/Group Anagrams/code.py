47dd4b9ff6cf117b77f30f28c14ed80f61d405b0
bhavana-15 <157963061+bhavana-15@users.noreply.github.com>
2024-03-22 15:48:17 +0530

    Create code.py

---
+++ strings/Group Anagrams/code.py
@@ -0,0 +1,13 @@
+from collections import Counter
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        m={}
+        n=len(strs)
+        for i in strs:
+            x=sorted(i)
+            y="".join(x)
+            if y not in m:
+                m[y]=[i]
+            else:
+                m[y].append(i)
+        return m.values()
